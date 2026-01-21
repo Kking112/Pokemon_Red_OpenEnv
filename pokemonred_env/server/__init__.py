98309051922550ffd27326f987c0510ab4106ddf
Kevin King <kking112@gmail.com>
2026-01-21 12:41:20 -0500

    Set up dependencies, initialized openenv pokemonred env

---
+++ pokemonred_env/server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Pokemonred Env environment server components."""
+
+from .pokemonred_env_environment import PokemonredEnvironment
+
+__all__ = ["PokemonredEnvironment"]
